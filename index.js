2c165a6f795f274c1ceef6390b9ddd7d2b43df51
Glitch (glitch-hello-node) <none>
2023-12-06 16:15:54 +0000

    🐛🌺 Checkpoint ./package.json:274672/881 ./index.js:274672/345 ./public/index.html:274672/89

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ let express = require("express");
 let app = express();
 app.use(express.json());
 
-app.use('/', express.static('generator'));
+app.use('/', express.static('public'));
 
-const { Database } = require("quickmongo");
-// Writing the password here means it will be leaked, but for convenience...
-const db = new Database("mongodb+srv://yuqian:yuqianma@cluster0.v8pqot1.mongodb.net/?retryWrites=true&w=majority");
-db.on("ready", () => {
-    console.log("Connected to the database");
-});
-db.connect();
 
 let port = process.env.PORT || 3000;
-app.listen(port, ()=> {
-console.log('listening at ', port);
+app.listen(port, () => {
+  console.log('listening at ', port);
 });
