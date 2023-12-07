ae26c5b8c6bb56620d8c1bd2dc12f423ef3edf07
Glitch (glitch-hello-node) <none>
2023-12-07 13:44:19 +0000

    🚏📷 Checkpoint ./public/app.js:274672/1555 ./public/style.css:274672/4

--- public/app.js
+++ public/app.js
@@ -26,9 +26,31 @@ async function saveDataTo(url, data) {
 const saveDataToMongodb = (data) => saveDataTo('/api/data', data);
 const saveDataToServer = (data) => saveDataTo('/api/tmpdata', data);
 
-// if you want to keep the data even if the server stops:
+// [mongodb] if you want to keep the data even if the server stops:
 // saveDataToMongodb({ foo: 'foo' });
 
-// or you don't care it, just keep it this round (and you do not need to setup mongodb):
+// [server side tmp data] or you don't care it, just keep it this round (and you do not need to setup mongodb):
 // saveDataToServer({ bar: 'bar' });
+
+// [leaflet]
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const popup = L.popup()
+  .setLatLng([51.513, -0.09])
+  .setContent('I am a standalone popup.')
+  .openOn(map);
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent(`You clicked the map at ${e.latlng.toString()}`)
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
 
