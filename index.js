aa764e33e803f4d6c52529cd72ad348ca1cb94d5
Glitch (glitch-hello-node) <none>
2023-12-11 18:38:25 -0500

    style, markers and images

--- index.js
+++ index.js
@@ -1,65 +1,89 @@
 //initailization
 let express = require('express');
-const { Database } = require("quickmongo");
-
 let app = express();
+//configure express to parse json automatically in request bodies
 app.use(express.json());
 
-// set DB_URL in .env
-// user:pwd@cluster0.v8pqot1.mongodb.net
-const db = new Database('mongodb+srv://' + process.env.DB_URL + '/?retryWrites=true&w=majority');
-db.on("ready", () => {
-    console.log("Connected to the database");
-});
-db.on("error", (error) => {
-    console.error("Error with the database connection:", error);
-});
+//Import quickmongo 
+const { Database } = require("quickmongo");
+//create new database in Mongodb
+const db = new Database(`mongodb+srv://brooklynhumanist:5EO8UdybBu2Kkv6r@cluster0.x3shmbu.mongodb.net/?retryWrites=true&w=majority`)
+
+//connect to database
 db.connect()
 .then(() => console.log("Successfully connected to the database"))
    .catch(err => console.error("Database connection failed:", err));
 
+//listen for 'ready' event to confirm db is connected
+db.on("ready", async () => {
+    console.log("Connected to the database");
+//identify error with db connection
+});
+db.on("error", (error) => {
+    console.error("Error with the database connection:", error);
+});
+
+//removed this array as redundant to db use
+// let entriesData = [];
 
 
-let entriesData = [];
+// app.post('/entered', async (req, res) => {
+//     console.log("Received data at /entered:", req.body);
+//     // entries.push(req.body)
+//     // console.log(entries)
 
+//     try {
+        
+//         const result = await db.push('entriesData', req.body);
+//         console.log("Data pushed to database:", result);
+//         res.json({ task: "success" });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'An error occurred with the database operation.' });
+//     }    
+// });
 
+//defined post route for /entered to listen for data
 app.post('/entered', async (req, res) => {
     console.log("Received data at /entered:", req.body);
-    // entries.push(req.body)
-    // console.log(entries)
 
     try {
-        
+//attempt to push data to entriesData database
         const result = await db.push('entriesData', req.body);
         console.log("Data pushed to database:", result);
+//respond with success message
         res.json({ task: "success" });
     } catch (error) {
-        console.error(error);
+        console.error("Error during database operation:", error);
+        // Log detailed error information
+        if (error instanceof Error) {
+            console.error("Error stack:", error.stack);
+        } else {
+            console.error("Error object:", error);
+        }
         res.status(500).json({ error: 'An error occurred with the database operation.' });
-    }    
+    }
 });
 
-app.get('/remove-all-entries', async (req, res) => {
-  await db.set('entriesData', null);
-  res.json({ result: "success" });
-});
-
+//serve static client side files from public directory
 app.use('/', express.static('public'));
+
+//start server
 app.listen(1234, () => {
     console.log('listening:1234');
 });
 
-// app.get('/getEntries',(req,res)=>{
-//     let obj = {data:entries};
-//     res.json(obj);
-// })
+//define a Get route for /getEntries
 app.get('/getEntries', async (req, res) => {
     try {
+    //retrieve entriesData from the database
         const data = await db.get('entriesData');
         console.log("Data retrieved from database:", data);
+//set content type to json        
         res.setHeader('Content-Type', 'application/json');
+//respond with retrieved data
         res.json(data);
-        
+//identify and log any errors
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to retrieve entries.' });
--- public/app.js
+++ public/app.js
@@ -1,95 +1,170 @@
 
+
 const { OpenStreetMapProvider } = GeoSearch;
 
-async function callTest(locationQuery){
-    // setup
+//define geocode function
+async function callTest(locationQuery) {
+    // initialize OpenStreets
     const provider = new OpenStreetMapProvider();
-    // search
+    // search with locationquery
     const results = await provider.search({ query: locationQuery });
+    //return results and log them
     console.log(results)
     return results;
 }
 
-let Lat = 52.53092357796963;
-let Lon = 13.40350205203423;
 
+// iconUrl: 'dragonfruitMarker.png',
+// let entertainmentMarker = new dragonfruitMarker({iconUrl: 'DragonfruitEntertainment.png'}),
+//     foodMarker = new dragonfruitMarker({iconUrl: 'DragonfruitFood.png'}),
+//     historyMarker = new dragonfruitMarker({iconUrl: 'DragonfruitHistory.png'}),
+//     schoolMarker = new dragonfruitMarker({iconUrl: 'DragonfruitSchool.png'}),
+//     livingMarker = new dragonfruitMarker({iconUrl: 'DragonfruitLiving.png'});
+// to use: L.marker([lat, lon], {icon: foodMarker}).addTo(map).bindPopup("I am a green leaf.");
+
+
+let entertainmentMarker = L.icon({
+    iconUrl: 'DragonfruitEntertainment.png',
+    iconSize: [50, 75], // size of the icon
+    iconAnchor: [22, 75], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+let foodMarker = L.icon({
+    iconUrl: 'DragonfruitFood.png',
+    iconSize: [50, 75], // size of the icon
+    iconAnchor: [22, 75], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+let historyMarker = L.icon({
+    iconUrl: 'DragonfruitHistory.png',
+    iconSize: [50, 75], // size of the icon
+    iconAnchor: [22, 75], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+let schoolMarker = L.icon({
+    iconUrl: 'DragonfruitSchool.png',
+    iconSize: [50, 75], // size of the icon
+    iconAnchor: [22, 75], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+let livingMarker = L.icon({
+    iconUrl: 'DragonfruitLiving.png',
+    iconSize: [50, 75], // size of the icon
+    iconAnchor: [22, 75], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+//NYU Berlin Location Hard Coded
+let Lat = 52.53818908578969;
+let Lon = 13.412658806197292;
+
+//initalize leaflet map looking at NYU Berlin with Zoom of 13
 let map = L.map('map').setView([Lat, Lon], 13);
-let marker = L.marker([52.53820520020355, 13.412629767779348]).addTo(map);
+//marker placed at NYU Berlin
+let marker = L.marker([Lat, Lon], { icon: schoolMarker }).addTo(map);
+// let marker = L.marker([Lat, Lon],).addTo(map);
 
 
+//initialize an array of places
 let places = [];
 
+//add OpenStreetMap tile layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-// L.Control.geocoder().addTo(map);
 
-// // Initialize Mapbox Geocoder
-// const geocoder = new MapboxGeocoder({
-//     accessToken: 'pk.eyJ1IjoiYnJvb2tseW5odW1hbmlzdCIsImEiOiJjbHBwbXdzY2gweWZwMmpvdjJxOW9ieXBsIn0.7DGJB1rn9hKS8QpLR_XI4Q', 
-//     types: 'place',
-//     placeholder: 'Search for your city',
-// });
-// geocoder.addTo('#location');
-// geocoder.on('result', (data) => {
-//     console.log(data);
-//     if (data.result) {
-//         const { text, center } = data.result;
-//         setLocation(text, center); // Function to handle geocoder results
-//     }
-// });
-
-// // Function to handle geocoder results
-// function setLocation(text, center) {
-//     document.getElementById("location").value = text;
-//     places.push({ lat: center[1], lon: center[0] });
-// }
-
-
+//event listener for what to do when the page is loaded
 window.addEventListener('load', () => {
+    //call function to load map markers immediately with page
+    fetchEntriesAndAddMarkers();
+    //when enter button is pressed do the following
     document.getElementById('button-enter')?.addEventListener('click', async () => {
+        //get the values from each entry field
         let user = document.getElementById("user").value;
         let location = document.getElementById("location").value;
         let description = document.getElementById("description").value;
         let category = document.getElementById("category").value;
-        let multimedia = document.getElementById("multimedia");
         if (multimediaInput.files.length > 0) {
-            let file = multimediaInput.files[0];}
+            let file = multimediaInput.files[0];
+        }
         console.log(user);
         console.log(location);
         console.log(description);
         console.log(category);
-        console.log(multimedia);
         // Handle the form submission with geocoded data
         let lastEnteredLocation = places.length > 0 ? places[places.length - 1] : null;
+        //make an object from the entry information
         let obj = {
             "user": user,
             "location": location,
             "description": description,
             "category": category,
-            "multimedia": multimedia,
+            // "multimedia": file,
             "lat": lastEnteredLocation ? lastEnteredLocation.lat : undefined,
             "lon": lastEnteredLocation ? lastEnteredLocation.lon : undefined
         };
+        // Convert the object to a JSON string.
         let jsonData = JSON.stringify(obj);
 
+        // Use the geocoding function to get coordinates for the provided location.
+
         const geocodeResults = await callTest(location);
+        // If geocoding results are returned, place a new marker on the map and update the object with the latitude and longitude.
+
         if (geocodeResults.length > 0) {
             const { x: lon, y: lat } = geocodeResults[0];
-            // You can use lat and lon as needed, for example:
-            // Add a marker to the map at the geocoded location
-            L.marker([lat, lon]).addTo(map).bindPopup(location);
+            let categoryIcon;
+
+            switch (category) {
+                case 'food':
+                    categoryIcon = foodMarker;
+                    break;
+                case 'entertainment':
+                    categoryIcon = entertainmentMarker;
+                    break;
+                case 'school-event':
+                    categoryIcon = schoolMarker;
+                    break;
+                case 'historic-site':
+                    categoryIcon = historyMarker;
+                    break;
+                case 'living':
+                    categoryIcon = livingMarker;
+                    break;
+                default:
+                    console.error('Invalid category:', category);
+                    return; // Exit the function if the category is invalid
+            }
+
+            // Now use the categoryIcon when creating a marker
+            L.marker([lat, lon], { icon: categoryIcon }).addTo(map).bindPopup(`User: ${user} <br>Location: ${location} <br>Description: ${description}<br>Category: ${category}`);
+
+
+
+            // if (geocodeResults.length > 0) {
+            //     const { x: lon, y: lat } = geocodeResults[0];
+
+            // //if the entry is for living notate the area, if it is anything 
+            // //else place a specific marker
+            //     if (category === 'Living'){
+            //         let polygonCoords =[
+            //             [lat, lon],
+            //         ]
+            //     }
+            //     // You can use lat and lon as needed, for example:
+            //     // Add a marker to the map at the geocoded location
+            //     L.marker([lat, lon]).addTo(map).bindPopup(location);
             // console.log(location);
             // console.log(lat);
             // console.log(lon);
-
             obj.lat = lat;
             obj.lon = lon;
-            jsonData = JSON.stringify(obj);            
-        console.log("Data to be sent to server:",jsonData)
+            jsonData = JSON.stringify(obj);
         }
+        console.log("Data to be sent to server:", jsonData);
 
+        // Send a POST request to the '/entered' endpoint with the JSON data.
         fetch('/entered', {
             method: 'POST',
             headers: {
@@ -97,46 +172,68 @@ window.addEventListener('load', () => {
             },
             body: jsonData
         })
+            //parse response as json
             .then(res => {
-                return res.json
+                return res.json();
             })
-            .then(data => (console.log("Response from /entered:",data)))
+            //log response and catch any errors
+            .then(data => (console.log("Response from /entered:", data)))
             .catch(error => console.error("Error in fetch /entered:", error));
     })
-
+    //function up click of get-read-out button - this is a place holder for map places
     document.getElementById('get-read-out').addEventListener('click', () => {
-        fetch('getEntries')
-            .then(resp => resp.json())
-            .then(data => {
-                console.log("Data received from /getEntries:",data);
+        fetchEntriesAndAddMarkers();
+    });
 
-                data.forEach(entry => {
-                    if (typeof entry.lat === 'number' && typeof entry.lon === 'number') {
-                        L.marker([entry.lat, entry.lon]).addTo(map)
-                            .bindPopup(`User: ${entry.user}, Description: ${entry.description}`);
-                    } else {
-                        console.error('Invalid entry:', entry);
-                    }
-                });
+    function fetchEntriesAndAddMarkers() {
+        //Get request to the /getEntries
+        fetch('getEntries')
+            //parse json
+            .then(resp => resp.json())
+            //process the info from server    
+            .then(data => {
+                console.log("Data received from /getEntries:", data);
+                //if data is an array make a market for each entry
+                if (Array.isArray(data)) {
+
+
+                    data.forEach(entry => {
+                        if (typeof entry.lat === 'number' && typeof entry.lon === 'number') {
+                            let categoryIcon;
+                            // Determine the icon based on the category
+                            switch (entry.category) {
+                                case 'food':
+                                    categoryIcon = foodMarker;
+                                    break;
+                                case 'entertainment':
+                                    categoryIcon = entertainmentMarker;
+                                    break;
+                                case 'school-event':
+                                    categoryIcon = schoolMarker;
+                                    break;
+                                case 'historic-site':
+                                    categoryIcon = historyMarker;
+                                    break;
+                                case 'living':
+                                    categoryIcon = livingMarker;
+                                    break;
+                            }
+
+                            L.marker([entry.lat, entry.lon],{icon: categoryIcon}).addTo(map)
+                                .bindPopup(`User: ${entry.user} <br>Location: ${entry.location} <br>Description: ${entry.description}<br>Category: ${entry.category}`);
+                        } else {
+                            console.error('Invalid entry:', entry);
+                        }
+                    });
+                } else   // If data is not an array, log an error or handle it appropriately
+                {
+                    console.error('Expected an array of entries:', data);
+                }
             })
             .catch(error => {
                 console.error('Error fetching entries:', error);
             });
-            // .then(response => {
-            //     if (!response.ok) {
-            //         throw new Error(`HTTP error! status: ${response.status}`);
-            //     } else if (!response.headers.get('content-type')?.includes('application/json')) {
-            //         throw new Error("Not a JSON response");
-            //     }
-            //     return response.json();
-            // })
-            // .then(data => {
-            //     console.log("Data from /getEntries:", data);
-            // })
-            // .catch(error => {
-            //     console.error("Fetch error:", error);
-            // });
-    }) 
-})
+    }
 
 
+});
